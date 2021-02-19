52bc284729ebe42b354ba8b7922cd88dd9139b19
Oxana <62831704+oxyrud@users.noreply.github.com>
2021-02-19 20:41:16 +0300

    lesson 4

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
-let num = 1;
+let num = '0';
 
-let score =0;
+let score;
 
-const question1 = {'Вопрос какой-то'};
+const question1 = {
+	question: 'Вопрос какой-то',
+	correctAnswer: 'a'
+};1
 
-const question2 = {'Другой вопрос'};
+const question2 = {
+	question:'Другой вопрос',
+	correctAnswer: 'b'
+};
 
-const question3 = {'Ещё вопрос'};
+const question3 = {
+	question:'Ещё вопрос',
+	correctAnswer: 'c'
+};
+
+const arr = [question1,question2];
+
+arr.forEach((item) => {
+	console.log(item.correctAnswer);
+})
+
+arr.unshift(question3);
+
+arr.forEach((item) => {
+	if(item.correctAnswer === 'c') {
+		console.log(item.correctAnswer);
+	}
+})
+
+const question4 = {
+	question:'Ещё вопрос',
+	correctAnswer: '4'
+};
+
+newArr = [question1,question2,question3,question4];
+
+console.log(newArr);
