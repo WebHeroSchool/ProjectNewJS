ca0884526da42c9c26bea328f77379e4d78f9445
Oxana <62831704+oxyrud@users.noreply.github.com>
2021-02-25 17:21:52 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,31 +1,51 @@
 const question1 = {
 	question: 'Вопрос какой-то',
+	answer1:'b',
+	answer2:'c',
+	answer3:'d',
 	correctAnswer: 'a'
 };
 
 const question2 = {
 	question:'Другой вопрос',
+	answer1:'f',
+	answer2:'g',
+	answer3:'h',
 	correctAnswer: 'b'
 };
 
 const question3 = {
 	question:'Ещё вопрос',
+	answer1:'s',
+	answer2:'m',
+	answer3:'n',
 	correctAnswer: 'c'
 };
 
 const question4 = {
 	question:'Ещё вопрос',
+	answer1:'t',
+	answer2:'y',
+	answer3:'u',
 	correctAnswer: 'd'
 };
 
 let q = [question1,question2,question3,question4];
-let questions = document.getElementById('question');
+let num = 0;
 
 buildQuiz =(q) => {
+	num ++
 	
-    q.forEach((item) =>{ 
+    q.forEach((item,num) =>{ 
     	let questions = document.getElementById('question');
+    	let anwers = document.getElementById ('answer');
+
+         if (num <= q.length) {
+         	num = 0;
+         }
     	question.innerHTML+= ` ${item.question}`
+    	answer.innerHTML+= item.answer1 + item.answer2 + item.answer3 + item.correctAnswer+item.answer4
+       
     }) 
 }
 
